d66f457799f202a7998b8849788f2c75c856bbad
TEZT-USER <nfkbkhdhdbs@gmail.com>
2025-07-28 21:33:52 +0300

    Fix leftover code and compose screens

--- app/src/main/java/com/example/homehub/composables/BasicButton.kt
+++ app/src/main/java/com/example/homehub/composables/BasicButton.kt
@@ -1,3 +1,5 @@
+package com.example.homehub.composables
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
--- app/src/main/java/com/example/homehub/screens/config_screen/ScheduleScreen.kt
+++ app/src/main/java/com/example/homehub/screens/config_screen/ScheduleScreen.kt
@@ -1,15 +1,28 @@
 package com.example.homehub.screens.config_screen
 
-import BasicButton
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.homehub.R
+import com.example.homehub.composables.BasicButton
 import com.example.homehub.composables.BasicDivider
 import com.example.homehub.composables.BasicTitle
 import com.example.homehub.composables.BasicToggleSwitch
@@ -76,69 +89,74 @@ fun ScheduleScreen() {
                 BasicToggleSwitch(
                     isToggled = isDelayedAutocloseToggled,
                     onToggledChange = { isDelayedAutocloseToggled = it },
-                    @@ -95, 66 + 92, 62 @@ fun ScheduleScreen() {
-                        BasicToggleSwitch(
-                            isToggled = isCrossedAutocloseToggled,
-                            onToggledChange = { isCrossedAutocloseToggled = it },
-                            label = context.getString(R.string.mainGate_label_crossedAutoclose)
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
+                    label = context.getString(R.string.mainGate_label_delayedAutoclose)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                TextField(
+                    value = delayedAutocloseSeconds.toString(),
+                    onValueChange = { delayedAutocloseSeconds = it.trim().toIntOrNull() ?: 0 },
+                    label = { Text(context.getString(R.string.schedule_label_delayedAutoclose)) },
+                    modifier = Modifier.fillMaxWidth(),
+                    enabled = isEditMode
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                BasicToggleSwitch(
+                    isToggled = isCrossedAutocloseToggled,
+                    onToggledChange = { isCrossedAutocloseToggled = it },
+                    label = context.getString(R.string.mainGate_label_crossedAutoclose)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                TextField(
+                    value = crossedAutocloseSeconds.toString(),
+                    onValueChange = { crossedAutocloseSeconds = it.trim().toIntOrNull() ?: 0 },
+                    label = { Text(context.getString(R.string.schedule_label_crossedAutoclose)) },
+                    modifier = Modifier.fillMaxWidth(),
+                    enabled = isEditMode
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+            ) {
+                BasicButton(
+                    title = context.getString(if (isEditMode) R.string.common_button_save else R.string.common_button_edit),
+                    onClick = {
+                        if (isEditMode) {
+                            preferences.setPreference(
+                                Constants.PREFERENCE_MAIN_GATE_CONFIG_DELAYED_AUTOCLOSE_ENABLED,
+                                isDelayedAutocloseToggled
+                            )
+                            preferences.setPreference(
+                                Constants.PREFERENCE_MAIN_GATE_CONFIG_UNOBSTRUCTED_AUTOCLOSE_ENABLED,
+                                isCrossedAutocloseToggled
+                            )
+                            preferences.setPreference(
+                                Constants.PREFERENCE_MAIN_GATE_CONFIG_DELAYED_AUTOCLOSE_SECONDS,
+                                delayedAutocloseSeconds
+                            )
+                            preferences.setPreference(
+                                Constants.PREFERENCE_MAIN_GATE_CONFIG_UNOBSTRUCTED_AUTOCLOSE_SECONDS,
+                                crossedAutocloseSeconds
+                            )
 
-                        TextField(
-                            value = crossedAutocloseSeconds.toString(),
-                            onValueChange = { crossedAutocloseSeconds = it.trim().toInt() },
-                            label = { Text(context.getString(R.string.schedule_label_crossedAutoclose)) },
-                            modifier = Modifier.fillMaxWidth(),
-                            enabled = isEditMode
-                        )
-                    }
-
-                            Box (
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .align(Alignment.BottomCenter)
-                            ) {
-                        BasicButton(
-                            title = context.getString(if (isEditMode) R.string.common_button_save else R.string.common_button_edit),
-                            onClick = {
-                                if (isEditMode) {
-                                    preferences.setPreference(
-                                        Constants.PREFERENCE_MAIN_GATE_CONFIG_DELAYED_AUTOCLOSE_ENABLED,
-                                        isDelayedAutocloseToggled
-                                    )
-                                    preferences.setPreference(
-                                        Constants.PREFERENCE_MAIN_GATE_CONFIG_UNOBSTRUCTED_AUTOCLOSE_ENABLED,
-                                        isCrossedAutocloseToggled
-                                    )
-                                    preferences.setPreference(
-                                        Constants.PREFERENCE_MAIN_GATE_CONFIG_DELAYED_AUTOCLOSE_SECONDS,
-                                        delayedAutocloseSeconds
-                                    )
-                                    preferences.setPreference(
-                                        Constants.PREFERENCE_MAIN_GATE_CONFIG_UNOBSTRUCTED_AUTOCLOSE_SECONDS,
-                                        crossedAutocloseSeconds
-                                    )
-
-                                    Toasts.show(
-                                        context,
-                                        context.getString(R.string.settings_toast_saved)
-                                    )
-                                }
-                                isEditMode = !isEditMode
-                            },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(bottom = 32.dp, top = 16.dp, start = 16.dp, end = 16.dp)
-                        )
-                    }
+                            Toasts.show(context, context.getString(R.string.settings_toast_saved))
+                        }
+                        isEditMode = !isEditMode
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 32.dp, top = 16.dp, start = 16.dp, end = 16.dp)
+                )
             }
         }
     }
+}
 
-    @Preview
-    @Composable
-    fun ScheduleScreenPreview() {
-        HomeHubTheme {
-            ScheduleScreen()
-        }
+@Preview
+@Composable
+fun ScheduleScreenPreview() {
+    HomeHubTheme {
+        ScheduleScreen()
     }
+}
